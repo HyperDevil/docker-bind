c7e48d5bb0e1f01573eefcae40220abd2649968a
HyperDevil <HyperDevil@users.noreply.github.com>
2017-11-04 20:16:58 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 /etc/init.d/bind9 start
-/opt/getdns/bin/stubby -C /opt/getdns/etc/stubby/stubby.yml -v 2
+/usr/sbin/gosu getdns /opt/getdns/bin/stubby -C /opt/getdns/etc/stubby/stubby.yml -v 2
